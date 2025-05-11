ba21aca4a18d5c8573a25b0b29cfe26f908f3dbe
QuestoM <netanel@questo.media>
2025-05-11 20:35:08 +0300

    Version 2025-05-11_20-35-08

--- setup-publish.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing required dependencies...
-npm install date-fns --save
-echo.
-echo Dependencies installed successfully.
-echo.
-echo Running GitHub publishing script...
-node publish-to-github.js
-pause 
